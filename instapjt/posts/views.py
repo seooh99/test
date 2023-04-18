0a0f28dbfe5f39872635e0307eed02ba5671fc8b
임서희 <seooh212@gmail.com>
2023-04-18 16:51:46 +0900

    insta

--- instapjt/posts/views.py
+++ instapjt/posts/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
 
 # Create your views here.
 
 def index(req):
     
     return render(req, 'posts/index.html')
+
+
+def create(req):
+    if req.method == "POST":
+        content = req.POST.get('content')
+        post = Post(content = content)
+        post.save()
+        return redirect('posts:index')
+    else:
+        post = PostForm()
+        context = {
+            'post':post,
+        }
+        return render(req, 'posts/create.html', context)
